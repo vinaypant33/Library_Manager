17969770aaa3e0dce88b59855b5f37c5796fd4ac
Vinay Pant <vinaypant24@gmail.com>
2023-04-13 14:39:47 +0530

    Initial App done with basic functionalities

---
+++ Models/calculations.py
@@ -0,0 +1,8 @@
+""" Will only be used for the price calculation and won't access the database directly """
+# Importing the files for further working 
+
+import sqlite3
+import time
+
+
+# Function the get the details and share the list of the final calculation
